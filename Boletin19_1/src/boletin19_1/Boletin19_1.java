8d97f497d5ebb98b7840e4940e4657900e8e59d7
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-01-29 12:31:37 +0100

    Boletin19_1

---
+++ Boletin19_1/src/boletin19_1/Boletin19_1.java
@@ -0,0 +1,16 @@
+package boletin19_1;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Boletin19_1 {
+
+    public static void main(String[] args) {
+        Metodos obx=new Metodos();
+        int numeros[]=new int[6];
+        numeros=obx.crearArray(numeros);
+        obx.visualizarArray(numeros);
+    }
+    
+}
---
+++ Boletin19_1/src/boletin19_1/Metodos.java
@@ -0,0 +1,21 @@
+package boletin19_1;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Metodos {
+    public int[] crearArray(int[]lista){
+        for(int i=0;i<lista.length;i++){
+            lista[i]=(int) (Math.random()*50);
+        }
+        return lista;
+    }
+    public void visualizarArray(int[] lista){
+        int max=lista.length-1;
+        for(int i=0;i<lista.length;i++){
+            System.out.println(lista[max]);
+            max--;
+        }
+    }
+}
